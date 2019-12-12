c18ea47b55834831b9201efc97ab3ea51dcee689
Connor Brookes <vbrookesy@hotmail.co.uk>
2019-12-12 12:06:31 +0000

    json

--- rooms/script.js
+++ rooms/script.js
@@ -1,15 +1,58 @@
 const pink = {
-    capacity = 5,
-    feature = "Wheelchair accessable",
-    feature1 = "projector"
+    capacity: 5,
+    feature: "Wheelchair accessable",
+    feature1:"projector",
+    toJSON(){
+        return `This room is ${this.feature} and has a ${this.feature1} with a ${this.capacity} capacity`;
+    }
 }
+const dataObj = {
+    data: pink
+};
+
+console.log(JSON.stringify(dataObj))
+
 const orange = {
-    capacity = 10,
-    feature = "Phone",
-    feature1 = "TV screen"
+    capacity: 10,
+    feature: "phone",
+    feature1:"TV screen",
+    toJSON(){
+        return `This room has a ${this.feature} and has a ${this.feature1} with a ${this.capacity} person capacity`;
+    }
 }
+const dataObj1 = {
+    data: orange
+};
+
+console.log(JSON.stringify(dataObj1))
+
 const blue = {
-    capacity = 2,
-    feature = "phone",
-    feature1 = "Wheelchair accessable"
+    capacity: 2,
+    feature: "phone",
+    feature1:"Wheelchair accessable",
+    toJSON(){
+        return `This room has a ${this.feature} and is ${this.feature1} with only a ${this.capacity} person capacity`;
+    }
 }
+const dataObj2 = {
+    data: blue
+};
+
+console.log(JSON.stringify(dataObj2))
+
+
+
+
+
+
+
+// const orange = {
+//     capacity = 10,
+//     feature = "Phone",
+//     feature1 = "TV screen"
+// }
+// const blue = {
+//     capacity = 2,
+//     feature = "phone",
+//     feature1 = "Wheelchair accessable"
+// }

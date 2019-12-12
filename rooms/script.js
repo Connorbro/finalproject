2024b4420c651548f1d17ec9ed080de67f0ce1b2
Connor Brookes <vbrookesy@hotmail.co.uk>
2019-12-12 11:20:08 +0000

    started json

---
+++ rooms/script.js
@@ -0,0 +1,15 @@
+const pink = {
+    capacity = 5,
+    feature = "Wheelchair accessable",
+    feature1 = "projector"
+}
+const orange = {
+    capacity = 10,
+    feature = "Phone",
+    feature1 = "TV screen"
+}
+const blue = {
+    capacity = 2,
+    feature = "phone",
+    feature1 = "Wheelchair accessable"
+}
